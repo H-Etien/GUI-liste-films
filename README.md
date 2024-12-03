1dfc92c72ee9170dfe68c3fff66672989bbf8af7
unknown <huangetienne123@gmail.com>
2024-12-03 22:15:12 +0100

    originally posted on another repo : https://github.com/H-Etien/ICC-TFE/tree/GUI-cine

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# GUI-liste-films
+# Cine app
+
+An app that helps you keep track of the movies you've watched.
+
+## ![image](https://github.com/user-attachments/assets/74334707-9274-4539-97ac-7bcb5fee676e)
+
+### Description
+
+A GUI built with PySide6 that allows you to add and remove (multiple at once) movies, with a local database.
+
+-   Pyside6
+-   Json
+
+(Originally posted on the following repository : https://github.com/H-Etien/ICC-TFE/tree/GUI-cine)
+
+---
+
+### Run the Project
+
+1. `pip install -r requirement.txt`
+2. `py app.py`
+
+OR
+
+Inside the folder _Cinéma Club App_, you can laucnh with the _Cinéma Club.exe_ file.
+
+---
+
+### Add movie
+
+![Cinéma_Club_aLGqzxmQFk](https://github.com/user-attachments/assets/87def225-067e-4181-849b-2623149742ca)
+
+### Delete movie
+
+![Cinéma_Club_0HAUC3JKEh](https://github.com/user-attachments/assets/b157ddbc-9ef5-4978-852c-2a9cbaacac3d)
+
+---
+
+Made with the Udemy course of Houdon Thibault (Python : La Formation Complète 2024)
---
+++ app.py
@@ -0,0 +1,79 @@
+from pathlib import Path
+import json
+
+from PySide6 import QtWidgets as QW, QtCore
+
+from movie import get_movies, Movie
+
+CUR_DIR = Path.cwd()
+
+class App(QW.QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Ciné Club")
+        self.setup_ui()
+        self.populate_movies()
+        self.setup_connections()
+
+    def setup_ui(self):
+        self.layout = QW.QVBoxLayout(self)
+        
+        self.le_movieTitle = QW.QLineEdit()
+        self.btn_add_movie = QW.QPushButton("Ajouter un film")
+        self.lw_liste_film = QW.QListWidget()
+        self.lw_liste_film.setSelectionMode(QW.QListWidget.ExtendedSelection)
+        self.btn_delete_movie = QW.QPushButton("Supprimer le(s) film(s)")
+        
+        self.layout.addWidget(self.le_movieTitle)
+        self.layout.addWidget(self.btn_add_movie)
+        self.layout.addWidget(self.lw_liste_film)
+        self.layout.addWidget(self.btn_delete_movie)
+        
+    def populate_movies(self):
+        list_movies = get_movies()
+        
+        if list_movies is None:
+            return None
+        
+        self.lw_liste_film.clear()
+        for movie in list_movies:
+            lw_item = QW.QListWidgetItem(movie.title)
+            lw_item.setData(QtCore.Qt.UserRole, movie)
+            self.lw_liste_film.addItem(lw_item)
+    
+    def setup_connections(self):
+        self.le_movieTitle.returnPressed.connect(self.add_movie)
+        self.btn_add_movie.clicked.connect(self.add_movie)
+        self.btn_delete_movie.clicked.connect(self.remove_movie)
+    
+    def add_movie(self):
+        movie_title = self.le_movieTitle.text()
+        
+        if not movie_title:
+            return False
+        
+        m = Movie(movie_title)
+        resultat = m.add_to_movies()
+        if resultat:
+            self.populate_movies()
+    
+    def remove_movie(self):
+        for selected_item in self.lw_liste_film.selectedItems():
+            movie = selected_item.data(QtCore.Qt.UserRole)
+            movie.remove_from_movies()
+            self.lw_liste_film.takeItem(self.lw_liste_film.row(selected_item))
+
+
+movie_db_path = CUR_DIR / "data" / "movies.json"
+if not movie_db_path.exists():
+    p = CUR_DIR / "data"
+    p.mkdir(exist_ok=True)
+    p = p / "movies.json"
+    p.touch()
+    with open(movie_db_path, "w") as f:
+        json.dump([], f)
+
+app = QW.QApplication([])
+window = App()
+window.show()
+app.exec()
